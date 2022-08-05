0508f592d7017f2c817f8561a4955e8e366242ca
andrew-kol <110204316+andrew-kol@users.noreply.github.com>
2022-08-05 15:12:15 +0300

    выполнил ДЗ урок 2

--- Main.java
+++ Main.java
@@ -1,5 +1,69 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет ");
+        var box=5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        box = box * 3;
+        System.out.println(box);
+        box = box/4;
+        System.out.println(box);
+
+        var liftingCapacity = 50;
+        var stuffWeight = 20;
+        var capacityLeft = liftingCapacity - stuffWeight;
+        System.out.println("Ещё можно положить "+ capacityLeft+ " кг вещей");
+
+        var appleWeight = 2;
+        var orangesWeight = 3;
+        var fruitsWeight = appleWeight+orangesWeight;
+        System.out.println("Общий вес фруктов "+ fruitsWeight);
+
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatoesWeight = 2;
+        var cucumbersWeight = 2;
+        var peppersWeight = 2;
+        var zucchiniWeight = 2;
+        var vegetablesWeight = tomatoesWeight+cucumbersWeight+peppersWeight+zucchiniWeight;
+        var productsWeight = fruitsWeight+vegetablesWeight+meatWeight+waterWeight;
+        System.out.println("Общий вес продуктов "+ productsWeight+ " кг!");
+
+        var leftWeight = liftingCapacity-stuffWeight-productsWeight;
+        System.out.println("Места осталось "+ leftWeight+ " кг!");
+
+        productsWeight = productsWeight*2;
+        System.out.println("Теперь вес продуктов "+ productsWeight);
+
+        leftWeight = liftingCapacity-stuffWeight-productsWeight;
+        System.out.println("Теперь места осталось "+ leftWeight+ " кг!");
+
+        var overLoad = (stuffWeight+productsWeight)% liftingCapacity;
+        System.out.println("Перегруз на "+ overLoad+ " кг!");
+
+        var productsInOneCar = productsWeight/2;
+        System.out.println("Продуктов в одной машине теперь "+ productsInOneCar+ " кг!");
+
+        short bananas = 200;
+        System.out.println("Бананов "+ bananas +" кг");
+
+        float sugar = 3;
+        float onePortion = sugar/4;
+        System.out.println("Одна порция сахара весит " + onePortion+ " кг");
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a+b+c;
+        System.out.println(d);
+
+        float g = a+1f;
+        System.out.println(g);
+
+
+
+
     }
 }
